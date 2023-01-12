1f5f049e0824be09ccd1beab7f4bc9ed977b1d02
Faith Nguyen <faithc.nguyen@gmail.com>
2023-01-12 14:08:36 +0800

    fix-position-attr

--- src/components/SearchAppBar.js
+++ src/components/SearchAppBar.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { styled, alpha } from "@mui/material/styles";
-import { positions } from "@mui/system";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
